436a7813f2b0b1be3aeeab68bd35c9a5788d9daf
Aphelios28 <truongphamnhat2004@gmail.com>
2026-01-13 18:43:33 +0700

    add class tracker

---
+++ tracker/tracker.py
@@ -0,0 +1,151 @@
+import cv2
+import numpy as np
+from utils.bbox_utils import get_bbox_width, get_center_of_bbox
+from ultralytics import YOLO
+import supervision as sv
+import pickle
+import os
+
+class Tracker:
+    def __init__(self, model_path: str):
+        self.model = YOLO(model_path)
+        self.tracker = sv.ByteTrack(lost_track_buffer=120)
+    def detect_frames(self, frames):
+        batch_size = 20
+        detection = []
+        for i in range(0, len(frames), batch_size):
+            detection_batch = self.model.predict(frames[i:i+batch_size], conf = 0.1)
+            detection += detection_batch
+        return detection
+
+    def draw_ellipse(self, frame, bbox, color, track_id = None):
+        y2 = int(bbox[3])
+        x_center, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+
+        cv2.ellipse(
+            frame,
+            center=(x_center, y2),
+            axes= (width, int(0.35*width)),
+            angle=0.0,
+            startAngle=-45,
+            endAngle=235,
+            color=color,
+            thickness=2,
+            lineType=cv2.LINE_4
+        )
+
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2 - rectangle_height//2)
+        y2_rect = (y2 + rectangle_height//2)
+        if track_id is not None:
+            cv2.rectangle(frame, (int(x1_rect), int(y1_rect)), (int(x2_rect), int(y2_rect)) , color, cv2.FILLED)
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_rect), int(y1_rect + 15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0, 0),
+                2
+            )
+
+        return frame
+
+    def draw_traingle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x, _ = get_center_of_bbox(bbox)
+
+        triangle_point = np.array([
+            [x, y],
+            [x - 10, y - 20],
+            [x + 10, y - 20],
+        ])
+
+        cv2.drawContours(frame, [triangle_point], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_point], 0, (0, 0, 0), 2)
+
+        return frame
+    def get_object_tracks(self, frames, read_from_stub = False, stub_path = None):
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
+
+        detections = self.detect_frames(frames)
+
+        tracks = {
+            "player": [],
+            "ball": [],
+            "referee": []
+        }
+
+        for frame_nums, detection in enumerate(detections):
+            cls_name = detection.names
+            cls_name_inv = {v:k for k, v in cls_name.items()}
+
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            for object_id, class_id in enumerate(detection_supervision.class_id):
+                if cls_name[class_id] == 'goalkeeper':
+                    detection_supervision.class_id[object_id] = cls_name_inv["player"]
+            detection_with_track = self.tracker.update_with_detections(detection_supervision)
+
+            tracks["player"].append({})
+            tracks["referee"].append({})
+            tracks["ball"].append({})
+
+            for frame_detection in detection_with_track:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                if cls_id == cls_name_inv['player']:
+                    tracks["player"][frame_nums][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_name_inv['referee']:
+                    tracks["referee"][frame_nums][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_name_inv['ball']:
+                    tracks["ball"][frame_nums][track_id] = {"bbox":bbox}
+
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_name_inv['ball']:
+                    tracks["ball"][frame_nums][1] = {"bbox": bbox}
+
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(tracks, f)
+        return tracks
+
+    def draw_annotation(self, video_frame, tracks):
+        output_video_frames = []
+
+        for frame_nums, frame in enumerate(video_frame):
+            frame = frame.copy()
+
+            player_dict = tracks["player"][frame_nums]
+            ball_dict = tracks["ball"][frame_nums]
+            referee_dict = tracks["referee"][frame_nums]
+
+            for track_id, player in player_dict.items():
+                frame = self.draw_ellipse(frame, player["bbox"], (0, 0, 255), track_id)
+
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
+
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_traingle(frame, ball["bbox"], (0, 255, 0))
+
+            output_video_frames.append(frame)
+        return output_video_frames
